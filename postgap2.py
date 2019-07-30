fe259850487ee64fa0e42ad066332e1e5e90a75c
zgturan <turan.zelihagozde@gmail.com>
2019-07-30 02:59:16 +0100

    Update postgap2.py

--- postgap2.py
+++ postgap2.py
@@ -12,16 +12,22 @@ os.chdir(os.path.join('..',post_dir_result))
 df=pd.read_csv(post_dir_result+str('.tsv'), sep="\t")
 
 for a in range(df.shape[0]):
-    out = []
-    with open(df.ix[a][0],'r') as f:
-        res_out = cPickle.load(f)
-        for i in range(len(res_out)):
-            gene= res_out[i].gene.name
-            CLPP = res_out[i].collocation_posterior
-            for tissue, clpp in CLPP.items():
-                out.append([gene, tissue, clpp])
-                dff = pd.DataFrame(out)
-        print(df.ix[a][0])
-        dff.to_csv(df.ix[a][0] + '.csv',index=True, encoding='utf-8')
+  try:
+      out = []
+      with open(df.ix[a][0],'r') as f:
+          res_out = cPickle.load(f)
+          for i in range(len(res_out)):
+              gene= res_out[i].gene.name
+              CLPP = res_out[i].collocation_posterior
+              for tissue, clpp in CLPP.items():
+                  out.append([gene, tissue, clpp])
+                  dff = pd.DataFrame(out)
+          print(df.ix[a][0])
+          dff.to_csv(df.ix[a][0] + '.csv',index=True, encoding='utf-8')
+    
+  except IOError:
+    print 'key error'
+ 
+   
 
       
